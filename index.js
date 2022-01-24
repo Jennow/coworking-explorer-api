f5c07406c9dd5aa4ad65c23be0e0caae9cd5f540
Jenifer Prochnow <jeniferprochnow@web.de>
2022-01-24 01:34:39 +0100

    Use mongodb instead of api

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-const PORT    = 8000;
-const express = require('express');
-const axios   = require('axios');
-const cheerio = require('cheerio');
-const res     = require('express/lib/response');
-const cors    = require('cors')
-const cache   = require('memory-cache');
-const config  = require('./config');
+const PORT      = 8000;
+const express   = require('express');
+const axios     = require('axios');
+const cheerio   = require('cheerio');
+const res       = require('express/lib/response');
+const cors      = require('cors')
+const cache     = require('memory-cache');
+const config    = require('./config');
+const mongoose  = require("mongoose")
+const connectDB = require("./models/db")
+const database  = "coworking_explorer"
+const Spaces    = require("./models/Spaces");
+const { json } = require('express/lib/response');
+const { range } = require('express/lib/request');
 
-var app = express()
-
+var app         = express()
 var corsOptions = {
     origin: true,
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
+}
    
 app.use(cors(corsOptions));
 let memCache = new cache.Cache();
@@ -36,17 +41,13 @@ var cacheRequest = (duration) => {
     }
 }
 
-spaces = [];
-axios.get('https://coworkingmap.org/map/?format=json&key=LG7WsM7ufvhhTMLK3NJawkSH',)
-.then((response) => {
-    response.data.forEach(element => {
-        element.slug = element.url.replace(/(^https:\/\/coworkingmap.org\/)/gi, '').replace('\/', '');
-        spaces.push(element);
+app.get('/sync', (req, res) => {
+    
+    syncSpaces().then(function(count) {
+        res.json({count:count});
+    }).catch(err => {
+        console.log(err)
     });
-    cache.put('spaces', spaces);
-})  
-.catch(function (error) {
-    console.log(error)
 });
 
 app.get('/', cacheRequest(config.cacheTTL), (req, res) => {
@@ -62,50 +63,178 @@ app.get('/', cacheRequest(config.cacheTTL), (req, res) => {
 });
 
 app.get('/spaces', cacheRequest(config.cacheTTL), (req, res) => {
-    res.json(spaces);
+
+    let lat = parseFloat(req.query.lat);
+    let lng = parseFloat(req.query.lng);
+    let zoom = req.query.zoom;
+
+    if (!lat || !lng) {
+        lng = 10.000654;
+        lat = 53.550341;
+    }
+
+    if (!zoom) {
+        zoom = 12;
+    }
+
+    let range = 10 / zoom;
+    console.log(req.query);
+
+    connectDB("mongodb://127.0.0.1:27017/"+database)
+    Spaces.find(
+        { 
+            lat: 
+                {
+                    $gte: lat - range,
+                    $lte: lat + range,
+                },
+            lng:
+                {
+                    $gte: lng - range,
+                    $lte: lng + range
+                }
+        
+        }, (err, spaces)=>{ 
+        if(err) throw err
+        res.json(spaces);
+        // mongoose.disconnect()
+    })
 });
 
 app.get('/spaces/:spaceIdentifier', cacheRequest(config.cacheTTL), async(req, res) => {
     const spaceIdentifier = req.params.spaceIdentifier;
-    const space           = spaces.filter(space => spaceIdentifier === space.slug)[0];
+    connectDB("mongodb://127.0.0.1:27017/"+database)
 
-    axios.get(space.url)
-    .then(response =>  {
-        const html = response.data;
-        const $    = cheerio.load(html);
+    Spaces.findOne({identifier:spaceIdentifier}, (err, space)=>{ 
+        if(err) throw err
+        console.log(space)
 
-        space['subTitle']    = $('h2', html).first().text();
-        space['description'] = $('p.description', html).first().text();
-        let website          = $('a[itemprop="url"]', html).first().text().replace(/[^A-Z0-9\/\:\.\_\-\&\?]/ig, '');
-        if (!website.match(/^[a-zA-Z]+:\/\//)) {
-            website = 'http://' + website;
+        if (space.logo) {
+            res.json(space);
+            return;
         }
-        space['website']     = website;
-        space['coverPhoto']  = $('.space-image img', html).first().attr('src');
-        space['logo']        = $('.logo-space img[itemprop="logo"]', html).first().attr('src');
 
-        let prices = [];
+        axios.get(space.url).then(response =>  {
+            const html = response.data;
+            const $    = cheerio.load(html);
 
-        $('ul.table-list li', html).each(function() {
-            let priceTitle = $(this).children('.col-double').children('.title').children('strong').text();
-            let personHint = $(this).children('.col-double').children('.title').children('span').text().replace('\n', '');
-            let legend     = $(this).children('.col-double').children('.legend').text();
-            let price      = $(this).children('.price').text();
-            let frequency  = $(this).children('.frequency').text();
-            
-            prices.push({
-                priceTitle,
-                personHint,
-                legend,
-                price,
-                frequency
+            space['subTitle']    = $('h2', html).first().text();
+            space['description'] = $('p.description', html).first().text();
+            let website          = $('a[itemprop="url"]', html).first().text().replace(/[^A-Z0-9\/\:\.\_\-\&\?]/ig, '');
+            if (!website.match(/^[a-zA-Z]+:\/\//)) {
+                website = 'http://' + website;
+            }
+            space['website']     = website;
+            space['coverPhoto']  = $('.space-image img', html).first().attr('src');
+            space['logo']        = $('.logo-space img[itemprop="logo"]', html).first().attr('src');
+
+            let prices = [];
+            $('ul.table-list li', html).each(function() {
+                let priceTitle = $(this).children('.col-double').children('.title').children('strong').text();
+                let personHint = $(this).children('.col-double').children('.title').children('span').text().replace('\n', '');
+                let legend     = $(this).children('.col-double').children('.legend').text();
+                let price      = $(this).children('.price').text();
+                let frequency  = $(this).children('.frequency').text();
+                
+                prices.push({
+                    priceTitle,
+                    personHint,
+                    legend,
+                    price,
+                    frequency
+                });
             });
+
+            space['prices'] = prices;
+
+            try {
+                // Connect to database
+                Spaces.findOneAndUpdate(
+                    {
+                        identifier: space.identifier
+                    }, 
+                    space, 
+                    {
+                        upsert: true,
+                        new: true,
+                        setDefaultsOnInsert: true
+                    }, function(err, result) {
+                    if (err) throw err;
+                    console.log(result);
+                    console.log("Document " + result['id'] +  " updated!")
+                });
+            } catch(err) {
+            console.log(err);
+            }
+            res.json(space)
+        })
+        
+        // mongoose.disconnect()
+    })
+
+    return;
+    
+});
+
+async function syncSpaces() {
+    var spaces = [];
+    var count  = 0;
+
+    await axios.get('https://coworkingmap.org/map/?format=json&key=LG7WsM7ufvhhTMLK3NJawkSH',)
+    .then((response) => {
+        response.data.forEach(element => {
+            element.slug = element.url.replace(/(^https:\/\/coworkingmap.org\/)/gi, '').replace('\/', '');
+            spaces.push(element);
         });
 
-        space['prices'] = prices;
+        cache.put('spaces', spaces);
 
-        res.json(space)
+        for (const space of spaces) {    
+            connectDB("mongodb://127.0.0.1:27017/"+database)
+            let spaceData = {
+                id: space['ID'],
+                name: space['name'],
+                url: space['url'],
+                identifier: space['slug'],
+                smallCoverPhoto: space['cover-photo'],     // For Map
+                lat: space['map']['lat'],
+                lng: space['map']['lng'],
+                zoom: space['map']['zoom'],
+                streetNumber: space['map']['street_number'],
+                streetName: space['map']['street_name'],
+                streetNameShort: space['map']['street_name_short'],
+                city: space['map']['city'],
+                state: space['map']['state'],
+                postCode: space['map']['post_code'],
+                contry: space['map']['country'],
+                countryShort: space['map']['country_short'],
+            }
+    
+            try {
+                // Connect to database
+                Spaces.findOneAndUpdate(
+                    {
+                        identifier: spaceData.identifier
+                    }, 
+                    spaceData, 
+                    {
+                        upsert: true,
+                        new: true,
+                        setDefaultsOnInsert: true
+                    }, function(err, result) {
+                    if (err) throw err;
+                });
+                count++;
+            } catch(err) {
+                return err;
+            }
+        }
+        return count;
     })
-});
+    .catch(function (error) {
+        console.log(error)
+    });
+    return count;
+}
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
--- models/Spaces.js
+++ models/Spaces.js
@@ -1,10 +1,40 @@
-const mongoose   = require("mongoose");
+const mongoose = require("mongoose");
+const Double   = require('@mongoosejs/double');
+
 const Schema     = mongoose.Schema;
 var SpacesSchema = new Schema({ 
+    id: Number,
     name: String,
-    age: Number,
-    role: String 
+    identifier: String,
+    subTitle: String,
+    description: String,
+    url: String,
+    website: String,
+    logo: String,
+    largeCoverPhoto: String,
+    smallCoverPhoto: String, // For Map
+    lat: Double,
+    lng: Double,
+    zoom: Number,
+    streetNumber: String,
+    streetName: String,
+    streetNameShort: String,
+    city: String,
+    state: String,
+    postCode: String,
+    contry: String,
+    countryShort: String,
+    prices: [
+        {
+            priceTitle: String,
+            personHint: String,
+            legend: String,
+            price: String,
+            frequency: String
+        }
+    ]
 })
 
-var Spaces     = mongoose.model("Users", SpacesSchema)
+var Spaces     = mongoose.model("Spaces", SpacesSchema)
+
 module.exports = Spaces; 
